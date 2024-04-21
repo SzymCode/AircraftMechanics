80e5195b719e1d12b2c0c13847a211c42fa1e404
SzymCode <s.radomski19@gmail.com>
2024-04-21 15:34:46 +0200

    PrimeVue installed

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,15 @@
 import { createApp } from 'vue'
+
+import 'primevue/resources/themes/lara-light-blue/theme.css'
 import './style.css'
+
 import App from './App.vue'
 
-createApp(App).mount('#app')
+export const app = createApp(App)
+
+/**
+ *  App uses & mount
+ */
+import PrimeVue from 'primevue/config'
+
+app.use(PrimeVue).mount('#app')
